6d2b232929ed3976f660d4af0729efb5aff58bd8
David Gazdag <dgazdag86@gmail.com>
2024-09-30 14:19:38 +0200

    initial commit

---
+++ 02 - Fundamentals of Programming/05 - Comments/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // prints Hello World!
+        System.out.println("Hello world!"); // prints Hello World!
+        /*
+        This is a long multi line comment
+        Second line
+        Third line
+         */
+    }
+}
